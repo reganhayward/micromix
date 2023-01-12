e8864088746673f0369711c741777dab02a403a0
reganhayward <reganhayward@gmail.com>
2023-01-13 02:41:17 +1300

    Add files via upload

---
+++ Website/backend/plugins/Heatmap.py
@@ -0,0 +1,8 @@
+def main(parameters):
+    # upload_url = "https://hp-heatmap-frontend-44nub6ij6q-ez.a.run.app/"
+    #upload_url = "http://hiri-heatmap.test.fedcloud.eu/"
+    #upload_url = "http://localhost:1024/"
+    upload_url = "http://127.1.1.1:8081/"
+    print("heatmap.py")
+    print(parameters)
+    return upload_url+'?config='+str(parameters["db_entry_id"])
---
+++ Website/backend/visualize.py
@@ -0,0 +1,15 @@
+# This is the main point for visualizations.
+# Parse all relevant dataframes to this module and decide what plugin to use with route().
+
+def route(collection, df, plugin, db_entry_id):
+    import importlib
+    from bson.json_util import ObjectId
+    from pymongo import MongoClient
+    print("Loading plugin....")
+    plugin_module = importlib.import_module("plugins.{}".format(plugin['name']))
+    visualization = {}
+    visualization['plugin_name'] = plugin['name']
+    visualization['plugin_id'] = str(plugin['_id'])
+    visualization['link'] = plugin_module.main({"df":df, "db_entry_id": db_entry_id})
+    print('vis_links: ', visualization)
+    return visualization
