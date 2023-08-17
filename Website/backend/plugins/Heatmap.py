54c6c5e8cec079f1dfee0cb33a8b1bc044d28f70
reganhayward <reganhayward@gmail.com>
2023-08-17 19:10:42 +1200

    Update Heatmap.py to correct port

--- Website/backend/plugins/Heatmap.py
+++ Website/backend/plugins/Heatmap.py
@@ -2,7 +2,7 @@ def main(parameters):
     # upload_url = "https://hp-heatmap-frontend-44nub6ij6q-ez.a.run.app/"
     #upload_url = "http://hiri-heatmap.test.fedcloud.eu/"
     #upload_url = "http://localhost:1024/"
-    upload_url = "http://127.1.1.1:8081/"
+    upload_url = "http://127.1.1.1:8080/"
     print("heatmap.py")
     print(parameters)
     return upload_url+'?config='+str(parameters["db_entry_id"])
